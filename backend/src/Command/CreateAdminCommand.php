874a08de69f0d3e02fb92677de8b9fae5dc6a002
I. Moukafih <moukafih@hotmail.com>
2026-04-29 21:23:35 +0200

    feat: impl auth in backend

---
+++ backend/src/Command/CreateAdminCommand.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Command;
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+#[AsCommand(
+    name: 'app:create-admin',
+    description: 'Creates a new admin user.',
+)]
+class CreateAdminCommand extends Command
+{
+    public function __construct(
+        private EntityManagerInterface $entityManager,
+        private UserPasswordHasherInterface $passwordHasher
+    ) {
+        parent::__construct();
+    }
+
+    protected function configure(): void
+    {
+        $this
+            ->addArgument('email', InputArgument::REQUIRED, 'The email of the admin.')
+            ->addArgument('password', InputArgument::REQUIRED, 'The password of the admin.')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+        $email = $input->getArgument('email');
+        $password = $input->getArgument('password');
+
+        $user = $this->entityManager->getRepository(User::class)->findOneBy(['email' => $email]);
+
+        if ($user) {
+            $io->warning(sprintf('Email already used: %s', $email));
+            return Command::FAILURE;
+        }
+
+        $user = new User();
+        $user->setEmail($email);
+        $user->setRole('ROLE_ADMIN');
+        
+        $hashedPassword = $this->passwordHasher->hashPassword($user, $password);
+        $user->setPassword($hashedPassword);
+
+        $this->entityManager->persist($user);
+        $this->entityManager->flush();
+
+        $io->success(sprintf('Admin user %s was successfully created.', $email));
+
+        return Command::SUCCESS;
+    }
+}
